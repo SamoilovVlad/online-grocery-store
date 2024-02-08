b09f009dc1c266c4b0ed3628e71fc121d9238889
Vladyslav Samoilov <borova21@ukr.net>
2024-02-08 22:26:29 +0100

    final version 2.0

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Identity;
 using Shop_Mvc.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,9 +19,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddMemoryCache();
 builder.Services.AddHttpContextAccessor();
 
+string encryptedConnectionString = builder.Configuration.GetConnectionString("MyDbContext");
+byte[] key = new byte[] { 204, 19, 62, 166, 246, 42, 91, 86, 216, 49, 131, 212, 247, 61, 249, 19, 197, 198, 220, 211, 177, 130, 80, 243, 163, 48, 121, 128, 98, 20, 177, 57 };
+string decryptedConnectionString = DecryptAES(encryptedConnectionString, key);
+
+
 builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseMySql(
-        builder.Configuration.GetConnectionString("MyDbContext"),
+        decryptedConnectionString,
         new MySqlServerVersion(new Version(8, 0, 23))
     )
 );
@@ -37,6 +43,30 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
     options.HttpOnly = HttpOnlyPolicy.None;
     options.Secure = CookieSecurePolicy.None;
 });
+
+static string DecryptAES(string ciphertextBase64, byte[] key)
+{
+    byte[] ciphertext = Convert.FromBase64String(ciphertextBase64);
+
+    using (Aes aesAlg = Aes.Create())
+    {
+        aesAlg.Key = key;
+        aesAlg.IV = new byte[16]; // Initialization Vector (IV)
+
+        ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+        using (MemoryStream msDecrypt = new MemoryStream(ciphertext))
+        {
+            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            {
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+}
 
 
 var app = builder.Build();
